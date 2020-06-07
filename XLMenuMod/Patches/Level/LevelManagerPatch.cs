77fb4f51d1a7541f77ac4dd5a923ebd8d971ef0c
Bryan Taylor <mcbtay@gmail.com>
2020-06-07 14:09:34 -0400

    Look for the presence of an accompanying assembly along side the level when loading it.  The assembly must have the same name as the map.  Check to ensure the assembly's not loaded before loading it.

--- XLMenuMod/Patches/Level/LevelManagerPatch.cs
+++ XLMenuMod/Patches/Level/LevelManagerPatch.cs
@@ -1,5 +1,9 @@
 ﻿using Harmony12;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using XLMenuMod.Levels;
 
 namespace XLMenuMod.Patches.Level
@@ -12,6 +16,28 @@ namespace XLMenuMod.Patches.Level
             static void Postfix(ref List<string> __result)
             {
                 __result.AddRange(CustomLevelManager.LoadNestedLevelPaths());
+            }
+        }
+
+        /// <summary>
+        /// Some levels have custom script assemblies that go with them.  Find if an assembly exists that matches the maps file name, and load it if soo.
+        /// </summary>
+        [HarmonyPatch(typeof(LevelManager), nameof(LevelManager.LoadLevelScene))]
+        public static class LoadLevelScenePatch
+        {
+            static void Prefix(LevelInfo level)
+            {
+                var path = Path.GetDirectoryName(level.path);
+                var file = Path.Combine(path, level.name + ".dll");
+                if (Directory.Exists(path) && File.Exists(file))
+                {
+                    // Because other mods may load the assembly, check to ensure that it's not already loaded.
+                    var assembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => !x.IsDynamic && Path.GetFileName(x.Location).StartsWith(level.name));
+                    if (assembly == null)
+                    {
+                        Assembly.LoadFile(file);
+                    }
+                }
             }
         }
     }
