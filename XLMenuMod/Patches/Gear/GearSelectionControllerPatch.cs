34099183e4f5395c13e8d7851e224816e4933238
Bryan Taylor <mcbtay@gmail.com>
2020-06-19 22:31:37 -0400

    updating to use traverse to call the private method instead of duplicating the code

--- XLMenuMod/Patches/Gear/GearSelectionControllerPatch.cs
+++ XLMenuMod/Patches/Gear/GearSelectionControllerPatch.cs
@@ -23,16 +23,7 @@ namespace XLMenuMod.Patches.Gear
                 __instance.visibleGear.Clear();
                 __instance.visibleGear.AddRange(CustomGearManager.NestedCustomGear);
 
-                #region GearSelectionController.UpdateList() -- Can't call it because it's private.
-                int index = __instance.visibleGear.FindIndex(item => __instance.previewCustomizer.HasEquipped(item));
-                if (index < 0)
-                    index = __instance.listView.GetHighlightedIndex();
-                if (index < 0)
-                    index = 0;
-                EventSystem.current.SetSelectedGameObject((GameObject)null);
-                __instance.listView.SetItems(__instance.visibleGear);
-                __instance.listView.HighlightIndex(index);
-                #endregion
+                Traverse.Create(__instance).Method("UpdateList").GetValue();
             }
         }
     }
