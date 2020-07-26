9aa32d5091bd6c3813149ee79eab8d2c22e6b0a7
Bryan Taylor <mcbtay@gmail.com>
2020-07-26 11:19:45 -0400

    cleaning up so unnecessary code and updating the code to re display the custom gear.

--- XLMenuMod/Gear/CustomGearManager.cs
+++ XLMenuMod/Gear/CustomGearManager.cs
@@ -7,143 +7,102 @@ using XLMenuMod.Interfaces;
 
 namespace XLMenuMod.Gear
 {
-    public class CustomGearManager : CustomManager
-    {
-        private static CustomGearManager _instance;
-        public static CustomGearManager Instance => _instance ?? (_instance = new CustomGearManager());
+	public class CustomGearManager : CustomManager
+	{
+		private static CustomGearManager _instance;
+		public static CustomGearManager Instance => _instance ?? (_instance = new CustomGearManager());
 
-        public static int CurrentGearFilterIndex { get; set; }
+		public static int CurrentGearFilterIndex { get; set; }
 
-        public override void LoadNestedItems()
-        {
-            NestedItems.Clear();
+		public override void LoadNestedItems(object[] objectsToLoad = null)
+		{
+			NestedItems.Clear();
 
-            foreach (var gear in GearSelectionController.Instance.visibleGear)
-            {
-                var singleMaterialGear = gear as GearInfoSingleMaterial;
+			var gearToLoad = (GearInfo[]) objectsToLoad;
+			if (gearToLoad == null) return;
+			
+			foreach (var gear in gearToLoad)
+			{
+				var singleMaterialGear = gear as GearInfoSingleMaterial;
 
-                // For now all I saw was one texture change per gear type, so assuming first.
-                var textureChange = singleMaterialGear?.textureChanges?.FirstOrDefault();
-                if (textureChange == null) continue;
+				// For now all I saw was one texture change per gear type, so assuming first.
+				var textureChange = singleMaterialGear?.textureChanges?.FirstOrDefault();
+				if (textureChange == null) continue;
 
-                if (string.IsNullOrEmpty(textureChange.texturePath) || !textureChange.texturePath.StartsWith(SaveManager.Instance.CustomGearDir)) continue;
+				if (string.IsNullOrEmpty(textureChange.texturePath) || !textureChange.texturePath.StartsWith(SaveManager.Instance.CustomGearDir)) continue;
 
-                var textureSubPath = textureChange.texturePath.Replace(SaveManager.Instance.CustomGearDir + '\\', string.Empty);
+				var textureSubPath = textureChange.texturePath.Replace(SaveManager.Instance.CustomGearDir + '\\', string.Empty);
 
-                if (string.IsNullOrEmpty(textureSubPath)) continue;
+				if (string.IsNullOrEmpty(textureSubPath)) continue;
 
-                var folders = textureSubPath.Split('\\').ToList();
-                if (!folders.Any()) continue;
+				var folders = textureSubPath.Split('\\').ToList();
+				if (!folders.Any()) continue;
 
-                CustomFolderInfo parent = null;
-                if (folders.Count == 1 || Path.GetExtension(folders.First()).ToLower() == ".png")
-                {
-                    // This gear item is at the root.
-                    AddItem(singleMaterialGear, ref parent);
-                    continue;
-                }
+				CustomFolderInfo parent = null;
+				if (folders.Count == 1 || Path.GetExtension(folders.First()).ToLower() == ".png")
+				{
+					// This gear item is at the root.
+					AddItem(singleMaterialGear, ref parent);
+					continue;
+				}
 
-                parent = null;
-                foreach (var folder in folders)
-                {
-                    if (Path.GetExtension(folder).ToLower() == ".png")
-                    {
-                        AddItem(singleMaterialGear, ref parent);
-                    }
-                    else
-                    {
-                        AddFolder<CustomGearFolderInfo>(folder, Path.GetDirectoryName(textureChange.texturePath), ref parent);
-                    }
-                }
-            }
+				parent = null;
+				foreach (var folder in folders)
+				{
+					if (Path.GetExtension(folder).ToLower() == ".png")
+					{
+						AddItem(singleMaterialGear, ref parent);
+					}
+					else
+					{
+						AddFolder<CustomGearFolderInfo>(folder, Path.GetDirectoryName(textureChange.texturePath), ref parent);
+					}
+				}
+			}
 
-            NestedItems = SortList(NestedItems);
-        }
+			NestedItems = SortList(NestedItems);
+		}
 
-        public void SetCurrentFolder(CustomFolderInfo folder, bool showCustom)
-        {
-            CurrentFolder = folder;
+		public override List<ICustomInfo> SortList(List<ICustomInfo> gear)
+		{
+			UserInterfaceHelper.SetSortLabelText(ref SortLabel, ((GearSortMethod)CurrentSort).ToString());
 
-            SetGearList(showCustom);
-        }
+			List<ICustomInfo> sorted;
 
-        public void SetGearList(bool showCustom)
-        {
-            var gearSelector = FindObjectOfType<GearSelectionController>();
-            if (gearSelector != null && gearSelector.visibleGear != null)
-            {
-                if (showCustom)
-                {
-                    gearSelector.visibleGear.Clear();
+			switch (CurrentSort)
+			{
+				case (int)GearSortMethod.Newest:
+					sorted = gear.OrderBy(x => x.GetName() != "..\\").ThenByDescending(x => x.GetModifiedDate(false)).ToList();
+					break;
+				case (int)GearSortMethod.Oldest:
+					sorted = gear.OrderBy(x => x.GetName() != "..\\").ThenBy(x => x.GetModifiedDate(true)).ToList();
+					break;
+				case (int)GearSortMethod.Name_ASC:
+					sorted = gear.OrderBy(x => x.GetName() != "..\\").ThenBy(x => x.GetName()).ToList();
+					break;
+				case (int)GearSortMethod.Name_DESC:
+				default:
+					sorted = gear.OrderBy(x => x.GetName() != "..\\").ThenByDescending(x => x.GetName()).ToList();
+					break;
+			}
 
-                    if (CurrentFolder == null)
-                    {
-                        gearSelector.visibleGear.AddRange(NestedItems.Select(x => x.GetParentObject() as ICharacterCustomizationItem));
-                        gearSelector.gearTypeFiltering.gearCategoryButton.label.SetText("Custom " + gearSelector.gearTypeFiltering.GearFilters[CurrentGearFilterIndex].GetLabel());
-                    }
-                    else
-                    {
-                        gearSelector.visibleGear.AddRange(CurrentFolder.Children.Select(x => x.GetParentObject() as ICharacterCustomizationItem));
-                        if (Main.BlackSprites != null)
-                        {
-                            gearSelector.gearTypeFiltering.gearCategoryButton.label.spriteAsset = Main.BlackSprites;
-                            gearSelector.gearTypeFiltering.gearCategoryButton.label.SetText(CurrentFolder.GetName().Replace("\\", "<sprite=10> "));
-                        }
-                    }
-                }
-                
-                gearSelector.listView.UpdateList();
-            }
-        }
+			return sorted;
+		}
 
-        public override void UpdateLabel()
-        {
-            var gearSelector = FindObjectOfType<GearSelectionController>();
-            if (gearSelector == null) return;
+		public override void OnPreviousSort<T>()
+		{
+			base.OnPreviousSort<GearSortMethod>();
 
-            UserInterfaceHelper.SetCategoryButtonLabel(ref gearSelector.gearTypeFiltering.gearCategoryButton.label, CurrentFolder.GetName(), "Custom " + gearSelector.gearTypeFiltering.GearFilters[CurrentGearFilterIndex].GetLabel(), CurrentFolder == null);
-        }
+			EventSystem.current.SetSelectedGameObject(null);
+			GearSelectionController.Instance.listView.UpdateList();
+		}
 
-        public override List<ICustomInfo> SortList(List<ICustomInfo> gear)
-        {
-            UserInterfaceHelper.SetSortLabelText(ref SortLabel, ((GearSortMethod)CurrentSort).ToString());
+		public override void OnNextSort<T>()
+		{
+			base.OnNextSort<GearSortMethod>();
 
-            List<ICustomInfo> sorted;
-
-            switch (CurrentSort)
-            {
-                case (int)GearSortMethod.Newest:
-                    sorted = gear.OrderBy(x => x.GetName() != "..\\").ThenByDescending(x => x.GetModifiedDate(false)).ToList();
-                    break;
-                case (int)GearSortMethod.Oldest:
-                    sorted = gear.OrderBy(x => x.GetName() != "..\\").ThenBy(x => x.GetModifiedDate(true)).ToList();
-                    break;
-                case (int)GearSortMethod.Name_ASC:
-                    sorted = gear.OrderBy(x => x.GetName() != "..\\").ThenBy(x => x.GetName()).ToList();
-                    break;
-                case (int)GearSortMethod.Name_DESC:
-                default:
-                    sorted = gear.OrderBy(x => x.GetName() != "..\\").ThenByDescending(x => x.GetName()).ToList();
-                    break;
-            }
-
-            return sorted;
-        }
-
-        public override void OnPreviousSort<T>()
-        {
-            base.OnPreviousSort<GearSortMethod>();
-
-            EventSystem.current.SetSelectedGameObject(null);
-            Traverse.Create(FindObjectOfType<GearSelectionController>())?.Method("UpdateList").GetValue();
-        }
-
-        public override void OnNextSort<T>()
-        {
-            base.OnNextSort<GearSortMethod>();
-
-            EventSystem.current.SetSelectedGameObject(null);
-            Traverse.Create(FindObjectOfType<GearSelectionController>())?.Method("UpdateList").GetValue();
-        }
-    }
+			EventSystem.current.SetSelectedGameObject(null);
+			GearSelectionController.Instance.listView.UpdateList();
+		}
+	}
 }
