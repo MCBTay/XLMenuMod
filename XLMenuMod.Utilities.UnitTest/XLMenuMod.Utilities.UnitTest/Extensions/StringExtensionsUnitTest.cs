6583307ddf0679a5cba902cb9227b0917b345a9c
Bryan Taylor <mcbtay@gmail.com>
2021-11-07 20:49:54 -0500

    adding more unit tests with / instead of \\

--- XLMenuMod.Utilities.UnitTest/XLMenuMod.Utilities.UnitTest/Extensions/StringExtensionsUnitTest.cs
+++ XLMenuMod.Utilities.UnitTest/XLMenuMod.Utilities.UnitTest/Extensions/StringExtensionsUnitTest.cs
@@ -9,7 +9,9 @@ namespace XLMenuMod.Utilities.UnitTest.Extensions
 	{
 		[Theory]
 		[InlineAutoMoqData("C:\\users\\username\\documents\\skaterxl\\gear\\folder1\\folder2\\somefile.png", "C:\\users\\username\\documents\\skaterxl\\gear", true)]
+		[InlineAutoMoqData("C:/users/username/documents/skaterxl/gear/folder1/folder2/somefile.png", "C:/users/username/documents/skaterxl/gear", true)]
 		[InlineAutoMoqData("C:\\users\\username\\documents\\skaterxl\\gear\\folder1\\folder2\\somefile.png", "C:\\users\\username\\documents\\skaterxl\\maps", false)]
+		[InlineAutoMoqData("C:/users/username/documents/skaterxl/gear/folder1/folder2/somefile.png", "C:/users/username/documents/skaterxl/maps", false)]
 		public void IsSubPathOfTests(string path, string baseDirPath, bool expected)
 		{
 			path.IsSubPathOf(baseDirPath).Should().Be(expected);
