78910eb3047d4e6f7b723eccc4e27200956b4840
Bryan Taylor <mcbtay@gmail.com>
2021-11-07 21:32:22 -0500

    Resolving More Code Smells (#29)
    * removing unnecessary case statements
    
    * removing unused params and marking class static
    
    * removing some commented out code
    
    * removing some commented out code and combining two ifs
    
    * removing more commented code
    
    * returning empty array instead of null
    
    * adding new keyword for intended hiding
    
    * simplifying nested ternary

--- XLMenuMod/Patches/Multiplayer/MultiplayerRoomInfoPatch.cs
+++ XLMenuMod/Patches/Multiplayer/MultiplayerRoomInfoPatch.cs
@@ -3,12 +3,12 @@ using XLMenuMod.Utilities.UserInterface;
 
 namespace XLMenuMod.Patches.Multiplayer
 {
-	public class MultiplayerRoomInfoPatch
+	public static class MultiplayerRoomInfoPatch
 	{
 		[HarmonyPatch(typeof(MultiplayerRoomInfo), nameof(MultiplayerRoomInfo.ConfigureListItemView))]
 		public static class ConfigureListItemViewPatch
 		{
-			static void Postfix(MultiplayerRoomInfo __instance, IndexPath index, ref MVCListItemView itemView)
+			static void Postfix(ref MVCListItemView itemView)
 			{
 				itemView.colors = Main.Settings.EnableDarkMode ? UserInterfaceHelper.DarkModeText : UserInterfaceHelper.DefaultText;
 			}
