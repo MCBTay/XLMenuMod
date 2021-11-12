c55c4c99f94a255fe7e1f797a7cc0a749cd18d82
Bryan Taylor <mcbtay@gmail.com>
2021-11-11 19:57:37 -0500

    updating assemblyinfo

--- XLMenuMod.Utilities/Properties/AssemblyInfo.cs
+++ XLMenuMod.Utilities/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("XLMenuMod.Utilities")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utilities for interacting with SkaterXL's menu system.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mcbtay")]
 [assembly: AssemblyProduct("XLMenuMod.Utilities")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
