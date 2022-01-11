673d108a60e54a22a568549d29091f3f01fc3eaa
mcbtay <mcbtay@gmail.com>
2022-01-10 22:11:43 -0500

    updates to ensure challenge list colors are correct regardless of light or dark mode

---
+++ XLMenuMod/Patches/Challenges/ChallengeListViewControllerPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using XLMenuMod.Utilities.UserInterface;
+
+namespace XLMenuMod.Patches.Challenges
+{
+    public static class ChallengeListViewControllerPatch
+    {
+        [HarmonyPatch(typeof(ChallengeListViewController), nameof(ChallengeListViewController.ConfigureListItemView))]
+        public static class UpdateListPatch
+        {
+            public static void Postfix(ref MVCListItemView itemView)
+            {
+                itemView.colors = Main.Settings.EnableDarkMode ? UserInterfaceHelper.DarkModeText : UserInterfaceHelper.DefaultText;
+            }
+        }
+    }
+}
