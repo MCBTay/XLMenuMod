2a34b6446aaffd84b3d955bdad2c8067fd7b5806
mcbtay <mcbtay@gmail.com>
2022-01-10 22:39:40 -0500

    adding a patch to ensure tutorial list items are the proper color in dark mode

---
+++ XLMenuMod/Patches/Tutorial/TutorialListControllerPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using XLMenuMod.Utilities.UserInterface;
+
+namespace XLMenuMod.Patches.Tutorial
+{
+    [HarmonyPatch(typeof(TutorialListController), nameof(TutorialListController.ConfigureListItemView))]
+    public static class ConfigureListItemViewPatch
+    {
+        public static void Postfix(ref MVCListItemView itemView)
+        {
+            itemView.colors = Main.Settings.EnableDarkMode ? UserInterfaceHelper.DarkModeText : UserInterfaceHelper.DefaultText;
+        }
+    }
+}
