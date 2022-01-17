860bc926f43a5b9a25525db51b7288a1eb2ad50d
mcbtay <mcbtay@gmail.com>
2022-01-17 11:19:24 -0500

    adding ProceduralMenuPagePatch.  handles *most* of the settings

---
+++ XLMenuMod/Patches/Settings/ProceduralMenuPagePatch.cs
@@ -0,0 +1,59 @@
+﻿using HarmonyLib;
+using System.Linq;
+using UnityEngine.UI;
+using XLMenuMod.Utilities.UserInterface;
+
+namespace XLMenuMod.Patches.Settings
+{
+    public static class ProceduralMenuPagePatch
+    {
+        [HarmonyPatch(typeof(ProceduralMenuPage), nameof(ProceduralMenuPage.UpdatePage))]
+        public static class UpdatePagePatch
+        {
+            static void Postfix(ProceduralMenuPage __instance)
+            {
+                if (__instance.items == null || !__instance.items.Any()) return;
+
+                foreach (var item in __instance.items)
+                {
+                    switch (item)
+                    {
+                        case FloatSliderItem floatSlider:
+                            floatSlider.selectable.colors = GetColors();
+                            break;
+                        case IntSliderItem intSlider:
+                            intSlider.selectable.colors = GetColors();
+                            break;
+                        case ColorSliderItem colorSlider:
+                            colorSlider.selectable.colors = GetColors();
+                            break;
+                        case EnumSliderItem enumSlider:
+                            enumSlider.selectable.colors = GetColors();
+                            break;
+                        case StringListSliderItem stringListSlider:
+                            stringListSlider.selectable.colors = GetColors();
+                            break;
+                        case StringListIntSliderItem stringListIntSlider:
+                            stringListIntSlider.selectable.colors = GetColors();
+                            break;
+                        case InputFieldItem inputField:
+                            inputField.selectable.label.color = GetColors().normalColor;
+                            break;
+                        case ButtonItem button:
+                            button.selectable.colors = GetColors();
+                            break;
+                        case ToggleItem toggle:
+                            toggle.selectable.colors = GetColors();
+                            break;
+
+                    }
+                }
+            }
+
+            private static ColorBlock GetColors()
+            {
+                return Main.Settings.EnableDarkMode ? UserInterfaceHelper.DarkModeText : UserInterfaceHelper.DefaultText;
+            }
+        }
+    }
+}
