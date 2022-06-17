b99f37100bb5e27c1b27208577a00ca59c9b81a7
mcbtay <mcbtay@gmail.com>
2022-06-17 11:16:38 -0400

    adding unit test proj back as a framework proj

---
+++ XLMenuMod.Utilities.UnitTest/Extensions/StringExtensionsUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using FluentAssertions;
+using XLMenuMod.Utilities.Extensions;
+using XLMenuMod.Utilities.UnitTest.Core;
+using Xunit;
+
+namespace XLMenuMod.Utilities.UnitTest.Extensions
+{
+	public class StringExtensionsUnitTest
+	{
+		[Theory]
+		[InlineAutoMoqData("C:\\users\\username\\documents\\skaterxl\\gear\\folder1\\folder2\\somefile.png", "C:\\users\\username\\documents\\skaterxl\\gear", true)]
+		[InlineAutoMoqData("C:/users/username/documents/skaterxl/gear/folder1/folder2/somefile.png", "C:/users/username/documents/skaterxl/gear", true)]
+		[InlineAutoMoqData("C:\\users\\username\\documents\\skaterxl\\gear\\folder1\\folder2\\somefile.png", "C:\\users\\username\\documents\\skaterxl\\maps", false)]
+		[InlineAutoMoqData("C:/users/username/documents/skaterxl/gear/folder1/folder2/somefile.png", "C:/users/username/documents/skaterxl/maps", false)]
+		public void IsSubPathOfTests(string path, string baseDirPath, bool expected)
+		{
+			path.IsSubPathOf(baseDirPath).Should().Be(expected);
+        }
+
+        [Fact]
+        public void WithEnding_NullString()
+        {
+            string test = null;
+            string expected = "otherString";
+
+            test.WithEnding(expected).Should().Be(expected);
+        }
+
+        [Fact]
+        public void Right_NullValue()
+        {
+            string test = null;
+
+            Action act = () => test.Right(5);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Right_LessThanZeroLength()
+        {
+            string test = "value";
+
+            Action act = () => test.Right(-1);
+
+            act.Should().Throw<ArgumentOutOfRangeException>();
+        }
+    }
+}
